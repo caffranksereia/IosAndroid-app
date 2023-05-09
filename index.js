12846e43b73e58577a97a0a7df1d3be97841b307
caffranksereia <fabio@yaayoo.com.br>
2023-05-09 14:03:58 -0300

    add globalstyles, themeprovider e router

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import Home from './src';
+import { GlobalStyle } from './components/_container/GlobalStyle-Container';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './_assets/theme/theme';
+import Router from './src/_config/routes/routes';
+
 export default function App() {
-	return <Home />;
+	<ThemeProvider theme={theme}>
+		<GlobalStyle />
+		<Router />
+	</ThemeProvider>;
 }
