0f4675cfd0b0876f5967fed95b5e14726d5f03f3
caffranksereia <fabio@yaayoo.com.br>
2023-05-09 14:12:24 -0300

    adicionado os components dos mesmos

---
+++ src/screens/Home/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import HomeComponents from '../../components/Home/Login-Components';
+
+export default function Home() {
+	return <HomeComponents />;
+}
---
+++ src/screens/Produtos/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ProdutoListComponents from '../../pages/Produtos/Produto_List/produt_list-Components';
+export default function Produtos() {
+	return <ProdutoListComponents />;
+}
