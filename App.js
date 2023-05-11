7729eeb8577f8064ac675687e2a879a1f57083b1
caffranksereia <fabio@yaayoo.com.br>
2023-05-11 19:48:32 -0300

    reorganizei o erro do app

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import { GlobalStyle } from './src/styled/themes/GlobalStyle-Container';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './src/_assets/theme';
-import Router from './src/_config/routes/routes';
+import Home from './src/screens/Home';
 
 export default function App() {
-	<ThemeProvider theme={theme}>
-		<GlobalStyle />
-		<Router />
-	</ThemeProvider>;
+	return <Home />;
 }
